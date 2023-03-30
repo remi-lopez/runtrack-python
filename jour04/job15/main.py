00c819bccc875bbf1e65805aaead948495efccc4
Zepolimer <contact.remilopez@gmail.com>
2023-03-30 14:57:42 +0200

    final job15

--- jour04/job15/main.py
+++ jour04/job15/main.py
@@ -1,13 +1,3 @@
-"""
-    Écrire un programme qui demande à l’utilisateur de fournir une première chaîne de
-    caractères, puis une seconde. Le programme affiche 1 si les 2 chaines sont identiques
-    ou 0 si les chaînes ne sont pas identiques. Les chaînes ne sont constituées que de
-    lettres minuscules. La deuxième chaîne de caractères peut contenir un ou plusieurs ‘ * ‘.
-    Chaque ‘ * ‘ peut remplacer 0 ou plusieurs caractères. Par exemple, si la chaîne 1 est
-    “laplateforme” et la chaine 2 “lap*”, le programme affiche 1 car l’ ‘ * ‘ remplace ‘
-    lateforme ‘. Si la chaîne 1 est “laplateforme” et la chaîne 2 “l*a*pla*te*form***e” le
-    programme renvoie 1 car les ‘ * ‘ ne remplace rien.
-"""
 
 
 def inputs():
@@ -22,40 +12,34 @@ def inputs():
             compare_str(a, b)
 
 
-def compare_char(c1, c2):
-    if c1 == c2:
-        return True
-    else:
-        if c2 == '*':
-            return True
+def str_to_arr(string: str):
+    nw = []
 
-        print('Not good for %s %s' % (c1, c2))
-        return False
+    for s in string:
+        nw.append(s)
+
+    return nw
 
 
-def compare_str(str1, str2):
-    new_str1 = []
-    new_str2 = []
+def compare_str(str1: str, str2: str):
+    str1_arr = str_to_arr(str1)
+    str2_arr = str_to_arr(str2)
 
-    for s in str1:
-        new_str1.append(s)
-
-    for s in str2:
-        if s == '*':
-            new_str2.append(s)
+    for x, y in zip(str1_arr, str2_arr):
+        if x == y:
+            res = True
             break
         else:
-            new_str2.append(s)
+            if y == '*' and str2.index(y) + 1 == len(str2):
+                res = True
+                break
+            elif y == '*' and str2.index(y) + 1 != len(str2):
+                new_list = []
+                [new_list.append(item) for item in str2 if item != '*']
+                compare_str(str1, ''.join(new_list))
+                break
 
-    print(new_str1)
-    print(new_str2)
-
-    for x, y in zip(new_str1, new_str2):
-        res = compare_char(x, y)
-
-        if res:
-            compare_char(x, y)
-        else:
+            res = False
             break
 
     print(res)
